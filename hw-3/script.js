2076ee5d37836123878bfd6954686674f93f7c7d
nurislam <github20243@gmail.com>
2024-02-20 18:34:59 +0300

    create hw-3

---
+++ hw-3/script.js
@@ -0,0 +1,81 @@
+// Задча 1 //
+// let variable = 10;
+
+// if (variable === 10) {
+//   console.log('Верно');
+// }else{
+//   console.log('Не верно');
+// }
+
+
+//Задача 2//
+// let apartmentnumber = parseInt(prompt("Введите номер квартиры"))
+
+// if (apartmentnumber >= 1 && apartmentnumber <= 20) {
+//   console.log("Кватира находится в первом подьездею");
+// } else if (  apartmentnumber >= 21 && apartmentnumber <= 48) {
+//   console.log("Кватира находится во втором подбезде.");
+// }else if (apartmentnumber >= 49 && apartmentnumber <= 90) {
+//   console.log("Кватира находится в третьем подьезде");
+// } else {
+//   console.log("В данном доме нет кватиры с таким номером.");
+// }
+
+//Задача 3//
+// let a = 3
+
+// if (a > 0 && a < 5) {
+//   console.log("Верно");
+// }else {
+//   console.log("Не верно");
+// }
+// Задача 4 //
+
+// const time = 11
+
+// // zadacha5//
+
+// const name1 = "Nursultan"
+
+// const name2 = "Nurislam"
+
+// const nema3 = "Syimyk"
+
+// if (time >=6  && time <= 12) {
+//     console.log(`Доброе утро ${name1}`);
+// } else if (time >=13 && time <= 18) {
+//     console.log(`Добрый день ${name2}`);
+// }else if (time >=18 && time <= 23) {
+//     console.log(`Добрый вечер ${nema3}`);
+// }
+
+// zadacha 6
+
+// let starik = 66
+
+// if (starik >= 6 && starik <= 65) {
+//     console.log("Скидка жок");
+// } else {
+//     console.log("Сидка 65%");
+// }
+
+// zadacha 7 
+
+// let randomazer = Math.random()
+
+// if (randomazer <= 1 && randomazer >= 2/4) {
+//     console.log("орёл");
+// } else {
+//     console.log("решка");
+// }
+
+// zadacha 8 
+
+// const number = 7;
+
+// if (number % 2 === 1) {
+//     console.log('Число чётное');
+// } else {
+//     console.log('Число нечётное');
+// }
+
