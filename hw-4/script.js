612c705813122084db377273c87b38714418d710
Nurislam Abdimalikov <98372q@gmail.com>
2024-02-23 17:24:27 +0600

    hw-4

---
+++ hw-4/script.js
@@ -0,0 +1,92 @@
+// zadacha №_1
+// function norepost () {
+//     alert("Асалому алейкум")
+// }
+
+// norepost()
+
+// zadacha №_2
+// const chislo = function  (a , b) {
+    
+// }
+// console.log(chislo(3 , 6));
+
+// const chislo1 = () => {
+
+// }
+
+
+// Задача №3
+
+// let doublin = function (number, vara) {
+//     return number * vara;
+// }
+// console.log(doublin(6, 5));
+
+// Задача №4
+
+// function greetUsers(name) {
+//     alert(`Привет ${name} !`);
+// }
+// greetUsers("Нурс");
+
+// Задача №5
+
+// const adds = (a, b) => a + b;
+
+// console.log(adds("Бугун кун ", "Жылуу"));
+
+// Задача №6
+
+// function examinations(numbers) {
+//     if (numbers % 2 === 0) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+// console.log(examinations(4));
+
+// Задача №7
+
+// function bigNumbers(params1, params3) {
+//     if (params2 === params2) {
+//         return "Алар окшош брат "
+//     }
+//     let ifElse = params2 >= params3 ? `${params2} Чон ${params3}` : `${params3} Кичекей ${params2}`;
+//     return ifElse;
+// }
+// console.log(bigNumbers(44, 44));
+
+// Задача №8
+
+// function getLengthes(str) {
+
+//     return str.length;
+
+// }
+// let myString = "Hello" + "World!";
+
+// let length = getLengthes(myString);
+
+// console.log(length); 
+
+// Задача №9
+
+// function greeta(name, age) {
+//     console.log(`Привет, ${name}! Тебе ${age} лет.`);
+// }
+
+// greeta("Нурислам", 18);
+
+// Задача №10
+
+// function calculets(number, numbers) {
+//     if (number > 10 && numbers > 10) {
+//         return number + numbers;
+//     } else {
+//         return number - numbers;
+//     }
+// }
+
+// console.log(calculets(15, 20));
